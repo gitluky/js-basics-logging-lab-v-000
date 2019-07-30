79e937c0c98630751799cec98d035fc34ad6d85c
Jonathan Luk <jluk87@gmail.com>
2019-07-30 15:57:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const driver = 'bob';
 function calledWithExactly (something) {
   console.log(something);
 }
+
+calledWithExactly('this code was called');
+calledWithExactly(driver);
+calledWithExactly(`${driver} is the driver variable name`);
